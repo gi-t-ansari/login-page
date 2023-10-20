b576c2edeb5ccd8fe1ba642fed791957439b1e65
Tanzeel Ansari <121967095+gi-t-ansari@users.noreply.github.com>
2023-10-20 18:22:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Validate username and password
+
+    if (username === "Rahul" && password === "password") {
+      setError("");
+      setIsSubmitted(true);
+    } else {
+      setError("Invalid username or password");
+      setIsSubmitted(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username"> Username: </label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password"> Password: </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
